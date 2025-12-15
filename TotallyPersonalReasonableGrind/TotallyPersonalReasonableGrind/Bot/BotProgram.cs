dbc3c67be34b6163f413c82915143574100c0b7e
Micka <>
2025-12-15 11:14:45 +0100

    commit everything

--- TotallyPersonalReasonableGrind/TotallyPersonalReasonableGrind/Bot/BotProgram.cs
+++ TotallyPersonalReasonableGrind/TotallyPersonalReasonableGrind/Bot/BotProgram.cs
@@ -6,7 +6,7 @@ namespace TotallyPersonalReasonableGrind.Bot;
 
 public class BotProgram
 {
-    private static DiscordSocketClient m_client = new();
+    public static DiscordSocketClient m_client = new();
     private CommandDispatcher.CommandDispatcher m_commandDispatcher = new();
 
     public static async Task<SocketApplicationCommand> CreateSlashCommand(SlashCommandProperties properties)
