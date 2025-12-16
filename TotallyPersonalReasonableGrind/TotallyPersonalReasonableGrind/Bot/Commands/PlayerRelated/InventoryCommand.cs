1d1acf8f922fece4b1e83f9dc05c946889c0f140
Micka <>
2025-12-16 10:00:18 +0100

    inventory done

--- TotallyPersonalReasonableGrind/TotallyPersonalReasonableGrind/Bot/Commands/PlayerRelated/InventoryCommand.cs
+++ TotallyPersonalReasonableGrind/TotallyPersonalReasonableGrind/Bot/Commands/PlayerRelated/InventoryCommand.cs
@@ -10,41 +10,55 @@ namespace TotallyPersonalReasonableGrind.Bot.Commands.PlayerRelated;
 
 public class InventoryCommand : ICommand
 {
-    public ulong Id { get; } = ICommand.Index++; 
-    
+    public class InventoryMessageBuilder
+    {
+        public List<Inventory> Inventories = new();
+        public InventoryMessageBuilder(List<Inventory> inventories) => Inventories = inventories;
+
+        public void WriteInventoryMessage(MessageProperties properties)
+        {
+            EmbedBuilder embed = new();
+            embed.WithTitle("Inventory");
+            foreach (var current in Inventories)
+            {
+                if (current.Quantity == 0) continue;
+                Item item = ItemAccess.GetItemById(current.ItemId).Result;
+                embed.AddField(new EmbedFieldBuilder().WithName("ㅤ").WithValue(item.Name + " " + Item.EmojiFromName(item)).WithIsInline(true));
+                embed.AddField(new EmbedFieldBuilder().WithName("ㅤ").WithValue(item.SellValue + " :coin:").WithIsInline(true));
+                embed.AddField(new EmbedFieldBuilder().WithName("ㅤ").WithValue("x" + current.Quantity).WithIsInline(true));
+            }
+
+            properties.Embed = embed.Build();
+        }
+    }
+
+    public ulong Id { get; } = ICommand.Index++;
+
     public static SocketApplicationCommand BuildProperties()
     {
         SlashCommandBuilder builder = new();
         builder.WithName("inventory").WithDescription("Show current inventory");
-        
+
         SlashCommandProperties properties = builder.Build();
-        
+
         var task = BotProgram.CreateSlashCommand(properties);
         task.Wait();
         var result = task.Result;
-        
+
         return result;
     }
+
 
     public Task<bool> OnSlashCommand(SocketSlashCommand command)
     {
         //Get Player Inventory
         var inv = InventoryAccess.GetInventoryData(command.User.GlobalName).Result;
-        
+        InventoryMessageBuilder builder = new(inv);
+
         // Display Inventory
-        EmbedBuilder embed = new();
-        embed.WithTitle("Inventory");
-        embed.WithAuthor(command.User.GlobalName, command.User.GetAvatarUrl());
-        foreach (var current in inv)
-        {
-            if (current.Quantity == 0) continue;
-            Item item = ItemAccess.GetItemById(current.ItemId).Result;
-            embed.AddField(new EmbedFieldBuilder().WithName("ㅤ").WithValue(item.Name).WithIsInline(true));
-            embed.AddField(new EmbedFieldBuilder().WithName("ㅤ").WithValue(item.SellValue).WithIsInline(true));
-            embed.AddField(new EmbedFieldBuilder().WithName("ㅤ").WithValue(current.Quantity).WithIsInline(true));
-        }
-        command.RespondAsync(embed: embed.Build());
-        
+
+        command.ModifyOriginalResponseAsync(builder.WriteInventoryMessage);
+
         return Task.FromResult(false);
     }
 
