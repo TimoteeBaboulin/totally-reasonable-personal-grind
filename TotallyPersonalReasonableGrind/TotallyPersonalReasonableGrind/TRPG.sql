e8ca9772f3cff18147a91f1ad22b273b96be823c
Arca Théo <121396182+theodu30@users.noreply.github.com>
2025-12-19 17:21:21 +0100

    Full db structures and datas

--- TotallyPersonalReasonableGrind/TotallyPersonalReasonableGrind/TRPG.sql
+++ TotallyPersonalReasonableGrind/TotallyPersonalReasonableGrind/TRPG.sql
@@ -29,7 +29,7 @@ CREATE TABLE `area` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `id_UNIQUE` (`id`),
   UNIQUE KEY `name_UNIQUE` (`name`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -38,7 +38,7 @@ CREATE TABLE `area` (
 
 LOCK TABLES `area` WRITE;
 /*!40000 ALTER TABLE `area` DISABLE KEYS */;
-INSERT INTO `area` VALUES (1,'Plain',0);
+INSERT INTO `area` VALUES (1,'Plain',0),(2,'Forest',2),(3,'Beach',4),(4,'Swamp',6),(5,'Ocean',8),(6,'Desert',10),(7,'Volcano',12),(8,'Sky',14);
 /*!40000 ALTER TABLE `area` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -60,7 +60,7 @@ CREATE TABLE `inventory` (
   KEY `FK_INVENTORY_ITEM_ID_idx` (`item_id`),
   CONSTRAINT `FK_INVENTORY_ITEM_ID` FOREIGN KEY (`item_id`) REFERENCES `item` (`id`) ON DELETE CASCADE,
   CONSTRAINT `FK_INVENTORY_PLAYER_ID` FOREIGN KEY (`player_id`) REFERENCES `player` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=48 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -69,7 +69,6 @@ CREATE TABLE `inventory` (
 
 LOCK TABLES `inventory` WRITE;
 /*!40000 ALTER TABLE `inventory` DISABLE KEYS */;
-INSERT INTO `inventory` VALUES (1,3,1,4),(2,3,2,4);
 /*!40000 ALTER TABLE `inventory` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -88,7 +87,7 @@ CREATE TABLE `item` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `id_UNIQUE` (`id`),
   UNIQUE KEY `name_UNIQUE` (`name`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=49 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -97,7 +96,7 @@ CREATE TABLE `item` (
 
 LOCK TABLES `item` WRITE;
 /*!40000 ALTER TABLE `item` DISABLE KEYS */;
-INSERT INTO `item` VALUES (1,'Mushroom',2,'mushroom'),(2,'Rock',1,'rock'),(3,'Bone',2,'bone');
+INSERT INTO `item` VALUES (1,'Mushroom',2,'mushroom'),(2,'Rock',2,'rock'),(3,'Grass',1,'leafy_green'),(4,'Slime',2,'green_circle'),(5,'Leather',3,'ox'),(6,'Wool',1,'sheep'),(7,'Wood',5,'wood'),(8,'Resin',4,'honey_pot'),(9,'Brown Mushroom',3,'brown_mushroom'),(10,'Flesh',3,'bacon'),(11,'Bone',4,'bone'),(12,'Spore',3,'bubbles'),(13,'Sea Shell',8,'shell'),(14,'Water',3,'droplet'),(15,'Crab',11,'crab'),(16,'Hermit Crab',9,'shell'),(17,'Red Slime',4,'red_circle'),(18,'Tanned Bone',7,'bone'),(19,'Kelp',15,'leafy_green'),(20,'Rotten Wood',10,'wood'),(21,'Lilypad',20,'lotus'),(22,'Pirannah',10,'tropical_fish'),(23,'Flooded Flesh',5,'bacon'),(24,'Scale',4,'feather'),(25,'Coral',10,'coral'),(26,'Plastic',4,'milk'),(27,'Fiber Optic Cable',25,'flute'),(28,'Fish',9,'fish'),(29,'Coral Scale',16,'coral'),(30,'Relic',30,'coin'),(31,'Sand',8,'desert'),(32,'Cactus',18,'cactus'),(33,'Oil',50,'oil'),(34,'Burned Flesh',9,'bacon'),(35,'Snake',16,'snake'),(36,'Web',40,'spider_web'),(37,'Volcanic Rock',10,'rock'),(38,'Obsidian',20,'rock'),(39,'Lava',65,'drop_of_blood'),(40,'Dragon Scale',75,'feather'),(41,'Magma Ball',23,'orange_circle'),(42,'Seared Bone',15,'bone'),(43,'Cloud',100,'cloud'),(44,'Star',150,'star'),(45,'Sun',500,'sun'),(46,'Wing',100,'wing'),(47,'Stelar Dust',50,'star2'),(48,'Halo',300,'innocent');
 /*!40000 ALTER TABLE `item` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -122,7 +121,7 @@ CREATE TABLE `loot` (
   KEY `FK_LOOT_AREA_ID_idx` (`area_id`),
   CONSTRAINT `FK_LOOT_AREA_ID` FOREIGN KEY (`area_id`) REFERENCES `area` (`id`) ON DELETE CASCADE,
   CONSTRAINT `FK_LOOT_ITEM_ID` FOREIGN KEY (`item_id`) REFERENCES `item` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=49 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -131,7 +130,7 @@ CREATE TABLE `loot` (
 
 LOCK TABLES `loot` WRITE;
 /*!40000 ALTER TABLE `loot` DISABLE KEYS */;
-INSERT INTO `loot` VALUES (1,1,1,2,'Walk','Common',0),(2,2,1,4,'Walk','Common',0),(3,3,1,4,'Hit','Common',0);
+INSERT INTO `loot` VALUES (1,1,1,2,'Walk','UnCommon',1),(2,2,1,3,'Walk','Common',0),(3,3,1,5,'Walk','Common',0),(4,4,1,2,'Hit','UnCommon',1),(5,5,1,2,'Hit','Common',0),(6,6,1,3,'Hit','Common',0),(7,7,2,2,'Walk','Common',2),(8,8,2,3,'Walk','UnCommon',2),(9,9,2,2,'Walk','UnCommon',3),(10,10,2,3,'Hit','Common',2),(11,11,2,2,'Hit','Common',2),(12,12,2,4,'Hit','UnCommon',3),(13,13,3,2,'Walk','Rare',4),(14,14,3,5,'Walk','Common',4),(15,15,3,1,'Walk','Rare',5),(16,16,3,1,'Hit','UnCommon',4),(17,17,3,3,'Hit','Common',5),(18,18,3,2,'Hit','Rare',5),(19,19,4,3,'Walk','Common',6),(20,20,4,2,'Walk','UnCommon',6),(21,21,4,2,'Walk','Rare',7),(22,22,4,2,'Hit','Rare',6),(23,23,4,3,'Hit','UnCommon',7),(24,24,4,4,'Hit','Common',6),(25,25,5,3,'Walk','Rare',8),(26,26,5,6,'Walk','UnCommon',8),(27,27,5,1,'Walk','Epic',9),(28,28,5,3,'Hit','Common',8),(29,29,5,2,'Hit','Rare',8),(30,30,5,1,'Hit','Epic',9),(31,31,6,4,'Walk','Common',10),(32,32,6,2,'Walk','Rare',10),(33,33,6,1,'Walk','Epic',11),(34,34,6,4,'Hit','UnCommon',11),(35,35,6,2,'Hit','Rare',10),(36,36,6,1,'Hit','Rare',11),(37,37,7,5,'Walk','UnCommon',12),(38,38,7,3,'Walk','Rare',12),(39,39,7,1,'Walk','Epic',13),(40,40,7,1,'Hit','Legendary',13),(41,41,7,3,'Hit','Rare',12),(42,42,7,4,'Hit','UnCommon',12),(43,43,8,2,'Walk','Rare',14),(44,44,8,3,'Walk','Epic',15),(45,45,8,1,'Walk','Legendary',15),(46,46,8,2,'Hit','Rare',14),(47,47,8,10,'Hit','Epic',15),(48,48,8,1,'Hit','Legendary',15);
 /*!40000 ALTER TABLE `loot` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -150,6 +149,7 @@ CREATE TABLE `player` (
   `exploration_exp` int DEFAULT NULL,
   `exploration_lvl` int DEFAULT NULL,
   `area_id` int unsigned DEFAULT NULL,
+  `money` int unsigned DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `id_UNIQUE` (`id`),
   UNIQUE KEY `name_UNIQUE` (`name`),
@@ -164,7 +164,7 @@ CREATE TABLE `player` (
 
 LOCK TABLES `player` WRITE;
 /*!40000 ALTER TABLE `player` DISABLE KEYS */;
-INSERT INTO `player` VALUES (1,'theodu30_',0,0,0,0,1),(3,'PiwoZz',0,0,0,0,1),(4,'TheMoonBeard',0,0,0,0,1);
+INSERT INTO `player` VALUES (1,'theodu30_',0,0,0,0,1,0),(3,'PiwoZz',0,0,0,0,1,0),(4,'TheMoonBeard',0,0,0,0,1,0);
 /*!40000 ALTER TABLE `player` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -177,4 +177,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-12-15 17:41:37
+-- Dump completed on 2025-12-19 17:20:26
